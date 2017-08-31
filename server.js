bda2cfee10ab60860046ab1f45ee76e34b8aa90d
Kashi Samaraweera <kashi@kashis.com.au>
2017-08-31 17:01:11 +1000

    Working client list management.

--- server.js
+++ server.js
@@ -7,43 +7,93 @@ const ballotSocket = socket.of('/ballot');
 
 // [(negative votes), (positive votes)]
 const votes = [0, 0];
+let connectedClients = [];
 
 let votesTotal = 0;
 
-// Tally votes as they come in.
+/**
+ * Manages the client connection.
+ */
 voteSocket.on('connect', (rtPollClient) => {
     const clientId = rtPollClient.id;
-    console.log(`Client connected: ${clientId}`);
     
+    voteSocket.emit('clientId', clientId);
     voteSocket.emit('voteAverage', (votes[1] - votes[0])/votesTotal);
     
+    // Add our client to the client list.
+    rtPollClient.on('voterId', (voterId) => {
+        let client = connectedClients.find((client) => client.voterId === voterId);
+        if (!client) {
+            client = {
+                id: clientId,
+                voterId: voterId,
+                votes: 0,
+                positive: 0,
+                negative: 0,
+                average: 0
+            };
+            connectedClients.push(client);
+        } else {
+            // Update the client ID
+            client.id = clientId;
+        }
+
+        ballotSocket.emit('clientList', connectedClients);
+        rtPollClient.on('castVote', (voteData) => {
+            voteCount = parseInt(voteData, 10) || 0;
+    
+            // Short-circuit if NaN or otherwise undefined.
+            if (voteCount === 0) return;
+    
+            // Limit the vote magnitude.
+            voteCount = Math.min(voteCount, 1);
+            voteCount = Math.max(voteCount, -1);
+    
+            if (voteCount === -1) votes[0]++;
+            if (voteCount === 1) votes[1]++;
+    
+            if (votes[0] === votes[1]) { votes[0] = 0; votes[1] = 0; }
+            
+            votesTotal++;
+            
+            client.votes++;
+            if (voteCount === -1) client.negative++;
+            if (voteCount === 1) client.positive++;
+            client.average = (client.positive - client.negative)/client.votes;
+    
+            connectedClients.map(client => client.totalAverage = (client.positive - client.negative)/votesTotal);
+    
+            ballotSocket.emit('clientList', connectedClients);
+            voteSocket.emit('voteAverage', (votes[1] - votes[0])/votesTotal);
+        });
+    });
+
     rtPollClient.on('disconnecting', (reason) => {
+        // connectedClients = connectedClients.filter((connectedCLient) => connectedCLient.id !== clientId);
+        ballotSocket.emit('clientList', connectedClients);
         console.log("Client disconnecting: ", reason);
     });
 
-    rtPollClient.on('castVote', (voteData) => {
-        voteCount = parseInt(voteData, 10) || 0;
-
-        // Short-circuit if NaN or otherwise undefined.
-        if (voteCount === 0) return;
-
-        // Limit the vote magnitude.
-        voteCount = Math.min(voteCount, 1);
-        voteCount = Math.max(voteCount, -1);
-
-        if (voteCount === -1) votes[0]++;
-        if (voteCount === 1) votes[1]++;
-
-        if (votes[0] === votes[1]) { votes[0] = 0; votes[1] = 0; }
-        
-        votesTotal++;
-
-        voteSocket.emit('voteAverage', (votes[1] - votes[0])/votesTotal);
-    });
 });
 
+/**
+ * Creates a new client object.
+ * @param {String} clientId The identifier for the client.
+ */
+function createClient(clientId) {
+    return {
+        id: clientId,
+        votes: 0,
+        positive: 0,
+        negative: 0,
+        average: 0
+    }
+}
+
 ballotSocket.on('connect', (ballotClient) => {
-    socket.clients((error, clients) => ballotClient.emit('clientList', clients));
+    const clientId = ballotClient.id;
+    ballotClient.emit('clientList', connectedClients);
+    console.log("Ballot server connected. ID: ", clientId);
 });
 
 console.log("Listening on port ", port);
